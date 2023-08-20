349f943cb6f1504d64fa805df346636c1f0ebb1a
Latifa Seidu <owusuah28@gmail.com>
2023-08-20 13:22:58 -0700

    update1

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <limits.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdarg>
+
+int main(void);
+int _printf(const char *format, ...);
+int printf(const char *format, ...);
+#endif            
